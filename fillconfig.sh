02a6d0705370f4dfb0c823d26140289ff500dae3
Python School <school@python.com>
2018-01-27 23:56:02 +0100

    Update fillconfig to compose dns

--- fillconfig.sh
+++ fillconfig.sh
@@ -1,6 +1,6 @@
 
 cat > ts3_query_http_api/configuration.py <<EOF
-ip = "ts3-1" #string - ip or hostname
+ip = "ts3" #string - ip or hostname
 port = 10011 #int - port
 client_login_name = 'serveradmin'
 client_login_password = 'NdXEAbEV'
