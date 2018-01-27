9e8554210244af27d849dd5341d8cca34a1469b2
Python School <school@python.com>
2018-01-27 23:48:42 +0100

    Add first submodules and docker-compose

---
+++ fillconfig.sh
@@ -0,0 +1,13 @@
+
+cat > ts3_query_http_api/configuration.py <<EOF
+ip = "ts3-1" #string - ip or hostname
+port = 10011 #int - port
+client_login_name = 'serveradmin'
+client_login_password = 'NdXEAbEV'
+client_nickname = 'eldupa'
+sid = 1 #int - server id
+api_user = 'test'
+api_password = 'test'
+
+EOF
+
